c73087c0a18f40918ec72ef854dc764170769076
RahulM-3 <rahulcode97@gmail.com>
2025-07-04 18:09:46 +0000

    login and signup init

---
+++ src/Loginsignup/Loginsignup.jsx
@@ -0,0 +1,24 @@
+import './Loginsignup.css'
+
+function Loginsignup() {
+  return (
+    <div class='container'>
+      <div class='header'>
+        <div class='text'>Signup</div>
+        <div class='underline'></div>
+      </div>
+      <div class='inputs'>
+        <div class='input'>
+            <img src='' alt=''></img>
+            <input type='text'></input>
+        </div>
+        <div class='input'>
+            <img src='' alt=''></img>
+            <input type='password'></input>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loginsignup
