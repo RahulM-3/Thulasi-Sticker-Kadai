f7ed4212f2d440cdcd9c4eb037df4ec3a9b4c75d
RahulM-3 <rahulcode97@gmail.com>
2025-07-06 16:59:22 +0000

    remove signin/signup form after logging in

--- src/Loginsignup/Loginsignup.jsx
+++ src/Loginsignup/Loginsignup.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import './Loginsignup.css';
+import { useFormState } from 'react-dom';
 
 function Loginsignup() {
+  const [signedIn, setSignedIn] = useState(false);
   const [isSignIn, setIsSignIn] = useState(true);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -18,7 +20,9 @@ function Loginsignup() {
     if (isSignIn) {
       try {
         const result = await window.firebaseAPI.signinUser(username, password);
-        console.log(result)
+        if(result.data.success) {
+          setSignedIn(username)
+        }
         setMessage(result.data.message);
       } 
       catch (err) {
@@ -28,6 +32,9 @@ function Loginsignup() {
     else {
       try {
         const result = await window.firebaseAPI.signupUser(username, password);
+        if(result.data.success) {
+          setSignedIn(username)
+        }
         setMessage(result.data.message);
       }
       catch (err) {
@@ -36,53 +43,64 @@ function Loginsignup() {
     }
   };
 
-  return (
+  if(!signedIn) {
+    return(
     <div id="auth-container">
-      <form id="signin-form" onSubmit={handleSubmit}>
-        <h2>{isSignIn ? 'Sign In' : 'Sign Up'}</h2>
-        
-        <div id="username-wrapper">
-          <label htmlFor="username">Username</label>  
-          { message === "Username already taken" || message === "User does not exist" && <label style={{ color: 'red', position: 'absolute', marginTop: "15px"}}>
-            { message }
-          </label>}
-        </div>
-        <input
-          type="text"
-          id="username"
-          placeholder="Enter username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
+        <form id="signin-form" onSubmit={ handleSubmit }>
+          <h2>{ isSignIn ? 'Sign In' : 'Sign Up'}</h2 >
 
-        <div id="password-wrapper">
-          <label htmlFor="password">Password</label>
-          {isSignIn && <a href="#" id="forgot-link">Forgot password?</a>}
-          { message === "Password should be 6 character long" || message === "Incorrect password" && <label style={{ color: 'red', position: 'absolute', marginTop: "30px"}}>
-            { message }
-          </label>}
-        </div>
-        <input
-          type="password"
-          id="password"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
+          <div id="username-wrapper">
+            <label htmlFor="username">Username</label>  
+            { (message === "Username already taken" || message === "User does not exist") && <label style={{ color: 'red', position: 'absolute', marginTop: "15px"}}>
+              { message }
+            </label> }
+          </div>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter username"
+            value={ username }
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
 
-        <button type="submit">{isSignIn ? 'Sign In' : 'Sign Up'}</button>
+          <div id="password-wrapper">
+            <label htmlFor="password">Password</label>
+            { isSignIn && <a href="#" id="forgot-link">Forgot password?</a> }
+            { (message === "Password should be 6 character long" || message === "Incorrect password") && <label style={{ color: 'red', position: 'absolute', marginTop: "30px"}}>
+              { message }
+            </label> }
+          </div>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            value={ password }
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
 
-        <p id="switch-auth">
-          {isSignIn ? 'New to Sticker Kadai?' : 'Already have an account?'}
-          <button type="button" id="switch-to-signup" onClick={handleToggle}>
-            {isSignIn ? 'Sign Up' : 'Sign In'}
-          </button>
-        </p>
-      </form>
-    </div>
-  );
+          <button type="submit">{ isSignIn ? 'Sign In' : 'Sign Up' }</button>
+
+          <p id="switch-auth">
+            { isSignIn ? 'New to Sticker Kadai?' : 'Already have an account?' }
+            <button type="button" id="switch-to-signup" onClick={ handleToggle }>
+              { isSignIn ? 'Sign Up' : 'Sign In' }
+            </button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+  
+  if(signedIn) {
+    return(
+      <div>
+        <p>Logged in as</p>
+        <h1>{ username }</h1>
+      </div>
+    );
+  }
 }
 
 export default Loginsignup;
